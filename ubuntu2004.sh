f3cba06febfb9a1b924d83845e7bd6bcc8baa276
fuzzle20 <66202601+fuzzle20@users.noreply.github.com>
2022-06-20 17:14:28 +0700

    Update ubuntu2004.sh

--- ubuntu2004.sh
+++ ubuntu2004.sh
@@ -1,5 +1,5 @@
 sudo apt update -y
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common nload htop nano -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt install docker-ce -y
